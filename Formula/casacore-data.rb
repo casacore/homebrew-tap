adf3bd2cbdc6c5e4d89d5aeb625f0c66fc7c3bf2
Gijs Molenaar <gijs@pythonic.nl>
2021-02-23 18:27:28 +0200

    homebrew style fix

--- Formula/casacore-data.rb
+++ Formula/casacore-data.rb
@@ -8,32 +8,28 @@ end
 class CasacoreData < Formula
   desc "Ephemerides and geodetic data for casacore measures (via Astron)"
   homepage "https://github.com/casacore/casacore"
-  head "ftp://ftp.astron.nl/outgoing/Measures/WSRT_Measures.ztar", :using => ZtarDownloadStrategy
+  head "ftp://ftp.astron.nl/outgoing/Measures/WSRT_Measures.ztar", using: ZtarDownloadStrategy
 
-  deprecated_option "use-casapy" => "with-casapy"
   option "with-casapy", "Use Mac CASA.App (aka casapy) data directory if found"
 
-  APP_DIR = Pathname.new "/Applications"
-  CASAPY_APP_NAME = "CASA.app"
-  CASAPY_APP_DIR = APP_DIR / CASAPY_APP_NAME
-  CASAPY_DATA = CASAPY_APP_DIR / "Contents/data"
+  deprecated_option "use-casapy" => "with-casapy"
+
+  APP_DIR = "/Applications".freeze
+  CASAPY_APP_NAME = "CASA.app".freeze
+  CASAPY_APP_DIR = (APP_DIR / CASAPY_APP_NAME).freeze
+  CASAPY_DATA = (CASAPY_APP_DIR / "Contents/data").freeze
 
   def install
     if build.with? "casapy"
-      if !Dir.exists? CASAPY_APP_DIR
+      if !Dir.exist? CASAPY_APP_DIR
         odie "--with-casapy was specified, but #{CASAPY_APP_NAME} was not found in #{APP_DIR}"
-      elsif !Dir.exists? CASAPY_DATA
+      elsif !Dir.exist? CASAPY_DATA
         odie "--with-casapy was specified, but data directory not found at #{CASAPY_DATA}"
       end
       prefix.install_symlink CASAPY_DATA
     else
       (prefix / CASAPY_DATA.basename).install Dir["*"]
     end
-  end
-
-  test do
-    Dir.exists? (prefix / CASAPY_DATA.basename / "ephemerides")
-    Dir.exists? (prefix / CASAPY_DATA.basename / "geodetic")
   end
 
   def caveats
@@ -43,5 +39,10 @@ class CasacoreData < Formula
     else
       "Installed latest Astron WSRT_Measures tarball to #{data_dir}"
     end
+  end
+
+  test do
+    Dir.exist? (prefix / CASAPY_DATA.basename / "ephemerides")
+    Dir.exist? (prefix / CASAPY_DATA.basename / "geodetic")
   end
 end
